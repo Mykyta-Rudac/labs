54daec540d9c3f8fb81f19acbfee2e05de5c42e6
Mykyta-Rudac <rudachenko.nikita@gmail.com>
2025-11-29 21:52:51 +0200

    chore: remove uncovered demo InfraService to restore coverage

--- NetSdrClientApp/Infrastructure/InfraService.cs
+++
@@ -1,8 +0,0 @@
-namespace NetSdrClientApp.Infrastructure
-{
-    // Small dummy service used to demonstrate architectural rule violation
-    public class InfraService
-    {
-        public string GetInfo() => "infra";
-    }
-}
